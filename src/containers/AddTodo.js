871804ac31c735a635ad9b428509c186c83fecc7
dmibod <Qwerty456>
2019-01-07 14:27:56 +0200

    fix

--- src/containers/AddTodo.js
+++ src/containers/AddTodo.js
@@ -6,7 +6,7 @@ const AddTodo = ({ dispatch }) => {
   let input;
 
   return (
-    <nav className="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
+    <nav className="navbar bg-dark navbar-dark fixed-top">
       <div className="container-fluid">
         <form
           className="input-group ml-auto mr-auto"

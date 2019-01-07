57c681e9d1e2b89e93ad8aada085b2d1e2f00422
dmibod <Qwerty456>
2019-01-07 02:32:02 +0200

    Updates

---
+++ precache-manifest.98dcd9cdd1f49aa8985d53fee974928c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1175c037c60d23bedf74",
+    "url": "/kanban-ui/static/js/runtime~main.1175c037.js"
+  },
+  {
+    "revision": "28729b88543c64d650ef",
+    "url": "/kanban-ui/static/js/main.28729b88.chunk.js"
+  },
+  {
+    "revision": "ffee8809065761ba760c",
+    "url": "/kanban-ui/static/js/1.ffee8809.chunk.js"
+  },
+  {
+    "revision": "28729b88543c64d650ef",
+    "url": "/kanban-ui/static/css/main.39444962.chunk.css"
+  },
+  {
+    "revision": "ffee8809065761ba760c",
+    "url": "/kanban-ui/static/css/1.b706506f.chunk.css"
+  },
+  {
+    "revision": "45a9ab077efc8fb57d0eccb69dfa2d4d",
+    "url": "/kanban-ui/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/kanban-ui/precache-manifest.df0f726abbace060c1c70f92c91a2088.js"
+  "/kanban-ui/precache-manifest.98dcd9cdd1f49aa8985d53fee974928c.js"
 );
 
 workbox.clientsClaim();

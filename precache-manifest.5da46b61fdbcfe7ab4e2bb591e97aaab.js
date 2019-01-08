4dad7a3199eb1eb3c30b02eac6a916520656d628
dmibod <Qwerty456>
2019-01-08 14:44:06 +0200

    Updates

--- precache-manifest.5da46b61fdbcfe7ab4e2bb591e97aaab.js
+++ precache-manifest.5da46b61fdbcfe7ab4e2bb591e97aaab.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = [
   {
-    "revision": "fee66e712a8a08eef5805a46892932ad",
-    "url": "/kanban-ui/static/media/fontawesome-webfont.fee66e71.woff"
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/kanban-ui/static/media/fontawesome-webfont.af7ae505.woff2"
   },
   {
-    "revision": "950243f75afc37e83c52",
+    "revision": "e440f0a0024607f2f1cc",
     "url": "/kanban-ui/static/css/main.e6923355.chunk.css"
   },
   {
@@ -20,16 +20,16 @@ self.__precacheManifest = [
     "url": "/kanban-ui/static/js/runtime~main.1175c037.js"
   },
   {
-    "revision": "950243f75afc37e83c52",
-    "url": "/kanban-ui/static/js/main.950243f7.chunk.js"
+    "revision": "e440f0a0024607f2f1cc",
+    "url": "/kanban-ui/static/js/main.e440f0a0.chunk.js"
   },
   {
     "revision": "674f50d287a8c48dc19ba404d20fe713",
     "url": "/kanban-ui/static/media/fontawesome-webfont.674f50d2.eot"
   },
   {
-    "revision": "af7ae505a9eed503f8b8e6982036873e",
-    "url": "/kanban-ui/static/media/fontawesome-webfont.af7ae505.woff2"
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/kanban-ui/static/media/fontawesome-webfont.fee66e71.woff"
   },
   {
     "revision": "b06871f281fee6b241d60582ae9369b9",
@@ -40,7 +40,7 @@ self.__precacheManifest = [
     "url": "/kanban-ui/static/css/1.897f96eb.chunk.css"
   },
   {
-    "revision": "d6a94116e82f0ffeb55303747fa489ae",
+    "revision": "0fd3e5d4fc8093724833f62395b5ba24",
     "url": "/kanban-ui/index.html"
   }
 ];

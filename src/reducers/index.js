f323bd6118311a6ff5a0da84b092b4b9d7bda875
dmibod <Qwerty456>
2019-05-17 17:58:23 +0300

    remove unused card reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,10 @@
 import { combineReducers } from 'redux'
 import authReducer from './authReducer';
 import boardReducer from './boardReducer';
-import cardReducer from './cardReducer';
 import visibilityFilter from './visibilityFilter'
 
 export default combineReducers({
   auth: authReducer,
   boards: boardReducer,
-  cards: cardReducer,
   visibilityFilter
 })

0baf0d55df8ac93f23cc8f9a5c2e652e0c164fa0
dmibod <bodnardm@gmail.com>
2023-04-01 14:19:54 +0300

    Improve label rendering

--- src/components/board/card/Desc.js
+++ src/components/board/card/Desc.js
@@ -2,16 +2,10 @@ import React from 'react';
 import Prop from './Prop';
 
 export default ({id, description}) => {
- var text = description || '';
- let body = text;
- if (IsJsArray(text)){
-   const arr = ParseJsArray(text);
+ let body = description || '';
+ if (IsJsArray(body)){
+   const arr = ParseJsArray(body);
    body = arr.map((i, idx) => (<Prop key={`${id}_${idx}`} {...{id:`${id}_${idx}`, _key:i.key, value:i.value}}/>));
- } else if (text.includes(':')){
-  body = text 
-  .split(";").filter(s => s != null && s.length > 0)
-  .map(s => s.split(':'))
-  .map((p, idx) => (<div key={`${id}_${idx}`} className="desc-container"><div className="label">{p[0]}:</div><div className="value">{p[1]}</div></div>));
  }
   return <div className="card-text small text-justify">{body}</div>
 };
--- src/components/board/card/Key.js
+++ src/components/board/card/Key.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 export default ({id, text, style}) => {
   text = text || "";
+  if (text.length > 0) text += ":";
   style = style || {};
   return (
-    <div className="label" style={style}>{text}:</div>
+    <div className="label" style={style}>{text}</div>
   ); 
 };

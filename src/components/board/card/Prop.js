0ab8ae1446642d89e88152bae1538e85b556eb71
dmibod <bodnardm@gmail.com>
2023-05-13 12:58:55 +0300

    allow multiple formatted values in card desc

--- src/components/board/card/Prop.js
+++ src/components/board/card/Prop.js
@@ -4,13 +4,19 @@ import Value from './Value';
 
 export default ({id, _key, value}) => {
   _key = _key || {};
-  _key.id = `${id}_key`;
+  _key.id = `${id}_k`;
   value = value || {};
-  value.id = `${id}_value`;
+  value.id = `${id}_v`;
+  var values = {};
+  if (Array.isArray(value)){
+    values = value.map((i, idx) => (<Value {...{id: `${value.id}_${idx}`, text:i.text, style:i.style}}/>));
+  } else {
+    values = (<Value {...{id: `${value.id}`, text: value.text, style: value.style}}/>);
+  }
   return (
     <div className="desc-container">
-      <Key {...{id: `${_key.id}_body`, text: _key.text, style: _key.style}}/>
-      <Value {...{id: `${value.id}_body`, text: value.text, style: value.style}}/>
+      <Key {...{id: `${_key.id}`, text: _key.text, style: _key.style}}/>
+      {values}
     </div>
   );
 };

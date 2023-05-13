0fcafe5199af73f457ca2dbd0b76593e36d01b44
dmibod <bodnardm@gmail.com>
2023-05-13 19:47:09 +0300

    Remove : from card prop-value

--- src/components/board/card/Key.js
+++ src/components/board/card/Key.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 export default ({id, text, style}) => {
   text = text || "";
-  if (text.length > 0) text += ":";
   style = style || {};
   return (
     <div className="label" style={style}>{text}</div>
